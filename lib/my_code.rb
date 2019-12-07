22157717df9d5eb611639a3865f269eff54294ff
Summer Rizzo <srainrizzo@gmail.com>
2019-12-07 20:21:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,41 @@
 # My Code here....
+
+def map_to_negatize (source)
+  index = 0
+  result = []
+  while index < source.count do
+    result << (source[index] * -1)
+    index += 1
+  end
+  result
+end
+
+def map_to_no_change (source)
+  index = 0
+  result = []
+  while index < source.count do
+    result << (source[index])
+    index += 1
+  end
+  result
+end
+
+def map_to_double (source)
+  index = 0
+  result = []
+  while index < source.count do
+    result << (source[index] * 2)
+    index += 1
+  end
+  result
+end
+
+def map_to_square (source)
+  index = 0
+  result = []
+  while index < source.count do
+    result << (source[index] ** 2)
+    index += 1
+  end
+  result
+end
